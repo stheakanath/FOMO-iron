1abf5b2ef10566c0fee3aa33cea87bf5f1b52d55
Sony Theakanath <stheakanath@gmail.com>
2016-04-13 22:18:47 -0700

    Set up Framework for linking backend with frontend

---
+++ iron/VibeProtocol/VibeProtocol/VibeProtocol.h
@@ -0,0 +1,13 @@
+//
+//  VibeProtocol.h
+//  VibeProtocol
+//
+//  Created by Kuriakose Sony Theakanath on 4/13/16.
+//  Copyright © 2016 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VibeProtocol : NSObject
+
+@end
---
+++ iron/VibeProtocol/VibeProtocol/VibeProtocol.m
@@ -0,0 +1,13 @@
+//
+//  VibeProtocol.m
+//  VibeProtocol
+//
+//  Created by Kuriakose Sony Theakanath on 4/13/16.
+//  Copyright © 2016 Kuriakose Sony Theakanath. All rights reserved.
+//
+
+#import "VibeProtocol.h"
+
+@implementation VibeProtocol
+
+@end
